b21cafd6b9b2029a4e1cea721cec2760dd0c597e
Roma Iswara <roma.iswara@sinarmasmining.com>
2023-06-03 08:56:24 +0700

    [add] asset path

---
+++ lib/constants/assetPath.dart
@@ -0,0 +1,11 @@
+class AssetPath {
+  static const String images = 'assets/images';
+
+  static const String bgCard = '$images/bg_card.png';
+  static const String car = '$images/car.png';
+  static const String food = '$images/food.png';
+  static const String logo = '$images/logo.png';
+  static const String logoName = '$images/logo_name.png';
+  static const String music = '$images/music.png';
+  static const String wallet = '$images/wallet.png';
+}
