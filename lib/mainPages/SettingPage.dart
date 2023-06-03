0bce18a6734bad59e294404d826b97518732060d
Roma Iswara <roma.iswara@sinarmasmining.com>
2023-06-03 11:42:24 +0700

    [add] setting page

---
+++ lib/mainPages/SettingPage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:nabung/constants/assetPath.dart';
+import 'package:nabung/widgets/menuItem.dart';
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: const Icon(
+            Icons.arrow_back_ios,
+          ),
+        ),
+        title: const Text('Back'),
+      ),
+      body: ListView(
+        children: [
+          const SizedBox(height: 8),
+          MenuItem(
+            icon: AssetPath.notification,
+            label: 'Notifications',
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
